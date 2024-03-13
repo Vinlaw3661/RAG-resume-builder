e89b4f48e5d085400c5d5c161fc0645cb46a366f
Vinlaw <vmudehwe3661@gmail.com>
2024-03-12 22:49:42 -0400

    Added more pages to app

---
+++ pages/About.py
@@ -0,0 +1,49 @@
+import streamlit as st
+
+st.title("What's the point of this app? 😒")
+
+st.write(
+    '''This is not meant to be an inspirational page, nor is it meant to sound like
+    a revolutionary Steve Jobs product reveal. The bottomline is this: applying for internships
+    is already a tideous process as is. Now imagine having to write a bunch of essays, add to
+    that customized cover letters for each role you are applying for? 
+    (Might as well stick to that campus job you have been avoiding). The shere burden and pressures
+    that come with such a repetitive process are 
+    '''
+)
+
+st.write(
+    '''
+    ### What can I get out of this?
+
+    Short answer? Basically anything related to the internship application process concerning essay
+    writing, resume adjustment, and cover letter editing. Instead of writing generic content that might
+    not relate to your experiences, personality and the nature of the role you are applying for,
+    Turbo Intern uses the following contexts to tailor-make responses:
+
+    1. Job Description (Retreived from URL link)
+    2. Resume
+    3. Cover Letter
+    4. Writing Style Samples
+    5. Additional Information
+
+'''
+)
+
+
+
+
+st.write('# ****1. Essay Writing****')
+st.write(
+
+)
+
+st.subheader('**2. Cover Letter Writing**')
+st.write(
+
+)
+
+st.subheader('**3. Cover Letter and Resume Improvement**')
+st.write(
+
+)
